8c5d541d69945e21127736ba64d3b05be9e40eeb
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2020-05-27 18:16:38 +0000

    code rewritten

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
 function scheduler(tasks,coolingTime){
     let tasksMap = new Map();
-    for(let i=0;i<tasks.length;i++){
-        let task=tasks[i];
-        if(tasksMap.get(task)===undefined){
-            tasksMap.set(task,1);
-        }
-        else{
-            let value=tasksMap.get(task);
-            value=value+1;
-            tasksMap.set(task,value);
-        }
+    tasks.forEach((e)=>tasksMap.set(e,tasksMap.get(e)===undefined?1:tasksMap.get(e)+1))
+    let entries=Array.from(tasksMap.entries());
+    entries.sort(function(a,b){return b[1]-a[1]});
+    let count=0;
+    while(entries.length!==0){
+     if(!entries.some((e)=>e[1]!==1)){
+       count+=entries.length;
+        break;
+      }
+      else{
+        count+=(coolingTime+1>=entries.length?coolingTime+1:entries.length);
+      }
+       entries.forEach((e)=>{e[1]=e[1]-1});
+       entries=(entries).filter((e)=>e[1]>0);
     }
-    let keys=Array.from(tasksMap.keys());
-    keys.sort(function(a,b){return tasksMap.get(b)-tasksMap.get(a)});
-    let tasksList=[];
-    let keyPushed=false;
-    let tasksListPushCount=0;
-    while(tasksMap.size!==0){
-        keyPushed=false;
-        for(let i=0;i<keys.length;i++){
-            if(!tasksList.includes(keys[i])){
-                tasksList.push(keys[i]);
-                tasksListPushCount++;
-                if(tasksList.length>coolingTime){
-                    tasksList.shift();
-                }
-                keyPushed=true;
-                let value=tasksMap.get(keys[i]);
-                value=value-1;
-                tasksMap.set(keys[i],value);
-                if(tasksMap.get(keys[i])===0){
-                    tasksMap.delete(keys[i]);
-                    i=i-1;
-                }
-                keys=Array.from(tasksMap.keys());
-            }
-        }
-        if(!keyPushed){
-            tasksList.push('idle');
-            tasksListPushCount++;
-            if(tasksList.length>coolingTime){
-                tasksList.shift();
-            }
-        }
-    }
-    return tasksListPushCount;
+     return count;
 }
 
- console.log(scheduler(["A","A","A","B","B"],4)===11);
+
+console.log(scheduler(["A","A","B","B","C","D"],2)===6);
+console.log(scheduler(["A","A","A","B","B"],4)===11);
 console.log(scheduler(["A","A","A","B","B","B"],3)===10);
 console.log(scheduler(["A","B","A","B","A","B"],3)===10);
 console.log(scheduler(["A","A","A","B","B","B"],2)===8);
@@ -65,6 +38,10 @@ console.log(scheduler(["A","B","C","D"],1)===4);
 console.log(scheduler(["A","A","A"],0)===3);
 console.log(scheduler(["A","A","A"],5)===13);
 console.log(scheduler(["A","A","B","B","A","A"],4)===16);
+
+
+
+
 
 
 // Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
@@ -87,3 +64,5 @@ console.log(scheduler(["A","A","B","B","A","A"],4)===16);
 // Constraints:
 // The number of tasks is in the range [1, 10000].
 // The integer n is in the range [0, 100].
+
+
